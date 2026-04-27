2e6e69ec19573ef26b50f4f3285e78b7c431bd4e
Michael <mikova@microsoft.com>
2026-04-27 11:03:59 +0300

    rmv

--- src/sempy_labs/semantic_model/__init__.py
+++ src/sempy_labs/semantic_model/__init__.py
@@ -6,15 +6,10 @@ from ._copilot import (
 from ._caching import (
     enable_query_caching,
 )
-from ._mini import (
-    mini_model_manager,
-)
-
 
 __all__ = [
     "approved_for_copilot",
     "set_endorsement",
     "make_discoverable",
     "enable_query_caching",
-    "mini_model_manager",
 ]
